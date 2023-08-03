f3d7cec9517ee0a037948c9b78051212848188df
Safrin <safrin.carter94@gmail.com>
2023-08-04 01:39:52 +0800

    fix service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if(in_array(env('APP_ENV'), ['staging', 'production'])) {
+            URL::forceScheme('https');
+        }
     }
 }
